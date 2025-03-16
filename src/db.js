dae7a2e892ad9f3abaea3ea246abfa47db0fd7b5
adnin <adnin@base.co.id>
2025-03-17 05:36:00 +0700

    change setting on db

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const isProduction = process.env.NODE_ENV === 'production';
-
+console.log('mana', process.env.DATABASE_URL)
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'mysql',
   logging: false,
